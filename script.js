480b3c9d2b366286ee913400781a0efaed0630b4
Abbey311 <y.abbey2@gmail.com>
2021-06-12 20:51:23 -0400

    search and clear buttons work

--- script.js
+++ script.js
@@ -3,63 +3,64 @@ console.log("page loaded");
 const apiKey = "0e79938c7b4f5732f4719af0e2ca605c";
 const loadMovies= document.querySelector(".load");
 const movieResults = document.querySelector("#movie-results");
-// const movieArea = document.getElementById('movie-area');
 const searchInput = document.getElementById('search-input');
 const searchForm = document.getElementById('search-form');
 let currentPage = 1;
 const limit = 24;
 const offset = currentPage*limit;
 var currentSearchTerm = '';
-const clearButton = document.querySelector('#clear');
+const clearButton = document.querySelector('.clear');
 
 
 async function searchMovies(searchQuery) {
     console.log("search");
-    const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchQuery}`);
-    const jsonResponse = await response.json();
-    let movies = jsonResponse.results.map(result => ({
-        id: result.id,
-        title: result.title,
-        posterPath: result.poster_path,
-        voteAvg: result.vote_average,
-    }))
-    console.log(movies);
-    return movies;
+    const response = await fetch("https://api.themoviedb.org/3/search/movie?api_key=" + apiKey + "&query=" + searchQuery + "&page=" + currentPage);
+    const responseData = await response.json();
+    const data = responseData.results
+    data.forEach(element => displayMovies(element));
 }
 
-// function clearMovies(){
-//     movieArea.innerHTML = ''
-//     currentPage = 1
-//     getMovies();
-// }
-// clearButton.addEventListener('click', clearMovies);
+function clearMovies(){
+    movieResults.innerHTML = ''
+    currentPage = 1
+    getMovies();
+    searchInput.value = '';
+}
+clearButton.addEventListener('click', clearMovies);
 
 async function getMovies(){
-    const apiUrl = "https://api.themoviedb.org/3/movie/now_playing?&api_key=" + apiKey + "&q=" + currentSearchTerm + "&limit=" + limit + "&offset="+ offset + "&language=en-US&page=" + currentPage;
+    const apiUrl = "https://api.themoviedb.org/3/movie/now_playing?&api_key=" + apiKey + "&q=" + currentSearchTerm + "&language=en-US&page=" + currentPage;
     const response = await fetch(apiUrl);
     const responseData = await response.json();
     const data = responseData.results
-    console.log("It works", data);
+    // console.log("It works", data);
     data.forEach(element => displayMovies(element));
 }
 
 function displayMovies(movies) {
-    // console.log(movies);
-    
-    movieResults.innerHTML+=`
+    // console.log("here", movies);
+
+    movieResults.innerHTML+= `
     <div class="individualMovie">
     <img src="https://image.tmdb.org/t/p/w500${movies.poster_path} "alt=${movies.title} width="200"/>
     <div class="tr"
-        <p class="movTitle" style="color: white; "> ${movies.title}</p>
-        <p class="movVote" id="rating" style="color: darkgrey; ">${movies.vote_average}⭐️</p>
+        <p style="color: white; "> ${movies.title}</p>
+        <p id="rating" style="color: darkgrey; ">${movies.vote_average}⭐️</p>
     </div>
     </div>
     `
 }
+
 function showMore(event){
     event.preventDefault();
     currentPage++;
-    getMovies();
+    if (searchInput.value == ''){
+        getMovies();
+    }
+    else {
+        currentPage++;
+        searchMovies(searchInput.value);
+    }
 } 
 
 loadMovies.addEventListener("click", showMore);
@@ -67,10 +68,9 @@ loadMovies.addEventListener("click", showMore);
 async function handleFormSubmit(event) {
     event.preventDefault();
     movieResults.innerHTML = '';
-    currentSearchTerm = searchInput.value;
-    const results = await searchMovies(currentSearchTerm);
+    searchTerm = searchInput.value;
+    const results = await searchMovies(searchTerm);
     displayMovies(results);
-    console.log(results);
     searchInput.value = '';
 
 }
